a655ab10e04905290458f545af41a529fac2f6f4
evgenypanor <pevgeny@gmail.com>
2015-07-06 20:22:13 +0300

    ddd2

--- bin/Android/Release/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Projects\Mobile\2015\CordovaWinJS\ParkingCharacterRequest\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Projects\Mobile\2015\CordovaWinJS\ParkingCharacterRequest\platforms\android\AndroidManifest.xml \
--- bin/Android/Release/MainActivity-release-unsigned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Projects\Mobile\2015\CordovaWinJS\ParkingCharacterRequest\platforms\android\ant-build\MainActivity-release-unsigned.apk : \
-C:\Projects\Mobile\2015\CordovaWinJS\ParkingCharacterRequest\platforms\android\ant-build\MainActivity.ap_ \
-C:\Projects\Mobile\2015\CordovaWinJS\ParkingCharacterRequest\platforms\android\ant-build\classes.dex \
-C:\Projects\Mobile\2015\CordovaWinJS\ParkingCharacterRequest\platforms\android\CordovaLib\ant-build\classes.jar \

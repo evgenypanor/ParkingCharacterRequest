9d0a3f011af3ba26ee5762bc7bedb56541a9d7c6
evgenypanor <pevgeny@gmail.com>
2015-07-07 18:56:06 +0300

    10

--- bld/Ripple/iOS/Debug/services/mobileServices/settings/tlvmobileapp.js
+++
@@ -1,7 +0,0 @@
-﻿// http://go.microsoft.com/fwlink/?LinkID=290993&clcid=0x409
-var tlvmobileappClient;
-document.addEventListener("deviceready", function () {
-    tlvmobileappClient = new WindowsAzure.MobileServiceClient(
-                    "https://tlvmobileapp.azure-mobile.net/",
-                    "vBacFUoseJfKkPLjcRvVWZvKsIBEoq95");
-});
